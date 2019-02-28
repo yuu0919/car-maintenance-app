88bb806e9d43a111fbf6b02b74e9029c72981ee6
yuu0919 <yw3055.jenesice@gmail.com>
2019-02-28 17:59:35 +0900

    Complete commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,18 +5,6 @@ class UsersController < ApplicationController
   def index
   end
 
-  def show
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
   def profile
   end
 end
